04b7e624d8cd9689874ba8c5c3cbb1e308f85267
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-09-17 10:44:44 +0800

    Feat: Add suggestion

---
+++ src/main/kotlin/cn/xor7/easefieldtypein/KotlinFieldCompletionContributor.kt
@@ -0,0 +1,34 @@
+package cn.xor7.easefieldtypein
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.editor.CaretModel
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.lombok.utils.decapitalize
+import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
+
+
+class KotlinFieldCompletionContributor : CompletionContributor() {
+    override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
+        val editor: Editor = parameters.editor
+        val caretModel: CaretModel = editor.caretModel
+        val offset = caretModel.offset
+        val lineNumber: Int = editor.document.getLineNumber(offset)
+        val lineStartOffset: Int = editor.document.getLineStartOffset(lineNumber)
+        val lineEndOffset: Int = editor.document.getLineEndOffset(lineNumber)
+        val currentLineText: String = editor.document.getText(TextRange(lineStartOffset, lineEndOffset))
+        val cleanText = currentLineText.replace(" ", "").toLowerCaseAsciiOnly()
+        val beforeCursorText = editor.document.text.substring(lineStartOffset, offset)
+        val javaStyleFieldDeclaration = beforeCursorText.split(":").last().trim()
+        if (cleanText.contains("val:") || cleanText.contains("var:")) {
+            val fragments = javaStyleFieldDeclaration.split(" ").first().split("(?=[A-Z])".toRegex())
+            for ((index, _) in fragments.withIndex()) {
+                val completion = fragments.subList(index, fragments.size).joinToString("").decapitalize()
+                result.addElement(LookupElementBuilder.create(completion))
+            }
+        }
+    }
+}
